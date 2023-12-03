7923e850ec20d0b64eb46a3db45c1a3a1eeb322a
Josefina Albornoz <87395600+JoAlbornoz@users.noreply.github.com>
2023-12-03 03:24:23 -0300

    Add files via upload

---
+++ gestion_cultural.sql
@@ -0,0 +1,3 @@
+use gestion_cultural;
+insert into artista_detalles values (null,'Marta Minujin','Artes Visuales',null);
+insert into artista_detalles values (null,'Marta Minujin','Artes Visuales',null);
