79fd233479004cfb9c70afe5bbe0e1a7e0478434
Josefina Albornoz <87395600+JoAlbornoz@users.noreply.github.com>
2023-12-03 17:53:24 -0300

    Delete gestion_cultural.sql

--- gestion_cultural.sql
+++
@@ -1,3 +0,0 @@
-use gestion_cultural;
-insert into artista_detalles values (null,'Marta Minujin','Artes Visuales',null);
-insert into artista_detalles values (null,'Marta Minujin','Artes Visuales',null);
